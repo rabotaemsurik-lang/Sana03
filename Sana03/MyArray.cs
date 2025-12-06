9e35230e1222af9c3bab5a510f47dd457902e1d6
DenisHrytsai242 <ipz242_gdv@student.ztu.edu.ua>
2025-12-06 15:05:07 +0200

    Sana03 has been completed

---
+++ Sana03/MyArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana03
+{
+    public class MyArray
+    {
+        private double[] array;
+        public MyArray(double[] array) { 
+            this.array = array;
+        }
+        public double SumOfNegativeNumbers()
+        {
+            double sum_neg = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                    sum_neg += array[i];
+            }
+            return sum_neg;
+        }
+        public double MinElementOfArray() {
+            double min = array[0];
+            for (int i = 1; i < array.Length; i++) {
+                if (array[i] < min) min = array[i];
+            }
+            return min;
+        }
+        public int MaxIndextOfArray() {
+            int max = 0;
+            for (int i = 1; i < array.Length; i++) {
+                if (array[i] >  array[max]) max = i;
+            }
+            return max;
+        }
+        public double MaxModuleElementOfArray()
+        {
+            double max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i]) > Math.Abs(max))  max = array[i];
+            }
+            return max;
+        }
+        public double SumIndexOfPositiveElements()
+        {
+            double sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0) sum += i;
+            }
+            return sum;
+        }
+        public int IntegerNumnersInArray() {
+            int amount = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == (int)array[i]) amount++;
+            }
+            
+            return amount;
+        }
+
+
+    }
+}
---
+++ Sana03/Program.cs
@@ -0,0 +1,48 @@
+﻿using Sana03;
+
+internal class Program
+{
+    static int ReadIntNumberN()
+    {
+        int number;
+        while (!int.TryParse(Console.ReadLine(), out number) || number < 0) { 
+            Console.WriteLine("Введіть ціле число");
+        }
+        return number;
+
+    }
+    static double ReadDoubleElementOfArray()
+    {
+        double number;
+        while(!double.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Введіть коректно значення");
+        }
+        return number;
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть к-сть елементів масиву");
+        int N = ReadIntNumberN();
+        Console.WriteLine($"В масиві {N} елементів");
+        double[] arr = new double[N];
+        Console.WriteLine("Введіть елемнти масиву");
+        for(int i = 0; i < N; i++)
+        {
+            Console.WriteLine($"array[{i}] = ");
+            arr[i] = ReadDoubleElementOfArray();
+        }
+        MyArray myArray = new MyArray(arr);
+        Console.WriteLine("суму від’ємних елементів масиву :" + myArray.SumOfNegativeNumbers() );
+        Console.WriteLine("мінімальний елемент масиву :" + myArray.MinElementOfArray() );
+        Console.WriteLine("номер (індекс) максимального елемента масиву :" + myArray.MaxIndextOfArray());
+        Console.WriteLine("максимальний за модулем елемент масиву :" + myArray.MaxModuleElementOfArray());
+        Console.WriteLine("суму індексів додатних елементів :" + myArray.SumIndexOfPositiveElements());
+        Console.WriteLine("кількість цілих чисел у масиві :" + myArray.IntegerNumnersInArray());
+
+
+
+
+    }
+    
+}
